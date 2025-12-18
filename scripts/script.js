1213d077cb33ad78aecec0b725c9a268709f0cd4
Rojen Chakradhar <chakradhar@rojen.me>
2025-12-18 18:44:14 +0545

    new idea

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,64 @@
-const catImg = document.getElementsById('cat-img');
-async function fetchCat() {
-  catImg.src = "https://placehold.co/400x400?text-Fetching...";
-  try {
-    const response = await fetch('https://api.thecatapi.com/v1/images/search');
-    const data = await response.json();
-    catImg.src = data[0].url;
-  } catch (error) {
-    console.error('Oops, no cats today:', error);
-  }
+const holes = document.querySelectorAll(".hole");
+const scoreBoard = document.querySelector("#score");
+const timeBoard = document.querySelector("#time");
+const cats = document.querySelectorAll(".cat");
+
+let lastHole;
+let timeUp = false;
+let score = 0;
+let countdown;
+
+function randomTime(min, max) {
+	return Math.round(Math.random() * (max - min) + min);
 }
 
-function setVibe(vibe) {
-  catImg.className = '';
-  catImg.className.add('vibe-' + vibe);
+function randomHole(holes) {
+	const idx = Math.floor(Math.random() * holes.length);
+	const hole = holes[idx];
+	if (hole === lastHole) return randomHole(holes);
+	lastHole = hole;
+	return hole;
 }
 
-fetchCat();
+function peep() {
+	const time = randomTime(500, 1000);
+	const hole = randomHole(holes);
+	const cat = hole.querySelector(".cat");
+	cat.classList.add("up");
+
+	setTimeout(() => {
+		cat.classList.remove("up");
+		if (!timeUp) peep();
+	}, time);
+}
+
+function startGame() {
+	score = 0;
+	scoreBoard.textContent = 0;
+	timeUp = false;
+	let timeLeft = 30;
+	timeBoard.textContent = timeLeft;
+	document.getElementById("start-btn").disabled = true;
+
+	peep();
+
+	countdown = setInterval(() => {
+		timeLeft--;
+		timeBoard.textContent = timeLeft;
+		if (timeLeft <= 0) {
+			clearInterval(countdown);
+			timeUp = true;
+			alert("Game Over! Your cat score is: " + score);
+			document.getElementById("start-btn").disabled = false;
+		}
+	}, 1000);
+}
+
+function whack(e) {
+	if (!e.isTrusted) return;
+	score++;
+	this.classList.remove("up");
+	scoreBoard.textContent = score;
+}
+
+cats.forEach((cat) => cat.addEventListener("click", whack));
